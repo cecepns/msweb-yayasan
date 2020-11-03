f3efbe70758ce21b66d22edb44142da1ab33ff4b
cecepns <cecepns0429@gmail.com>
2020-11-03 10:54:59 +0700

    push1

--- pages/index.js
+++ pages/index.js
@@ -14,33 +14,37 @@ function Home({data}) {
 
             <div className="flex bg-gray-200 px-12">
                 <div className="flex flex-wrap justify-center lg:justify-between">
-                    {data.slice(0,6).map(item => {
-
-                        let isi = item.isi_berita;
-                        if (isi.length > 100) {
-                            isi = isi.substr(0, 100) + '[...]'
-                        }
-
-                        return <div key={item.id} className={style.wrapper}>
-                            <div className={style.imgWrap}>
-                                <img
-                                    src={`https://marifatussalaam.org/assets/berita/${item.image}`}
-                                    alt="tidak di temukan"></img>
-                            </div>
-                            <div className={style.body}>
-                                <p className={style.title}>
-                                    {item.judul_berita}
-                                </p>
-
-                                <div
-                                    dangerouslySetInnerHTML={{
-                                    __html: isi
-                                }}
-                                    className={style.desc}/>
-                            </div>
-                        </div>
-
+                    {data.sort((a, b) => {
+                        return new Date(b.tanggal_berita) - new Date(a.tanggal_berita);
                     })
+                        .slice(0, 6)
+                        .map(item => {
+
+                            let isi = item.isi_berita;
+                            if (isi.length > 100) {
+                                isi = isi.substr(0, 100) + '[...]'
+                            }
+
+                            return <div key={item.id} className={style.wrapper}>
+                                <div className={style.imgWrap}>
+                                    <img
+                                        src={`https://marifatussalaam.org/assets/berita/${item.image}`}
+                                        alt="tidak di temukan"></img>
+                                </div>
+                                <div className={style.body}>
+                                    <p className={style.title}>
+                                        {item.judul_berita}
+                                    </p>
+
+                                    <div
+                                        dangerouslySetInnerHTML={{
+                                        __html: isi
+                                    }}
+                                        className={style.desc}/>
+                                </div>
+                            </div>
+
+                        })
 }
                 </div>
             </div>
@@ -49,7 +53,7 @@ function Home({data}) {
     )
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
     const res = await fetch(`https://marifatussalaam.org/Rest_api`)
     const data = await res.json()
     return {
