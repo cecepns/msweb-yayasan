7c04b24d88729a52a4589e60433de8b72b45040e
cecepns <cecepns0429@gmail.com>
2020-11-03 09:48:57 +0700

    exercises2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import HomePage from './component/home/home'
+// import HomePage from './component/home/home'
+import style from './component/home/news/news.module.scss'
 
-
-function Home( {data} ) {
+function Home({data}) {
     // console.log(data)
     return (
         <div>
@@ -10,19 +10,52 @@ function Home( {data} ) {
                 <title>CepyKun - Website created using Next Js</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            
-          <HomePage news={data}/>
+
+            <div className="flex bg-gray-200 px-12">
+                <div className="flex flex-wrap justify-center lg:justify-between">
+                    {data.map(item => {
+
+                        let isi = item.isi_berita;
+                        if (isi.length > 100) {
+                            isi = isi.substr(0, 100) + '[...]'
+                        }
+
+                        return <div key={item.id} className={style.wrapper}>
+                            <div className={style.imgWrap}>
+                                <img
+                                    src={`https://marifatussalaam.org/assets/berita/${item.image}`}
+                                    alt="tidak di temukan"></img>
+                            </div>
+                            <div className={style.body}>
+                                <p className={style.title}>
+                                    {item.judul_berita}
+                                </p>
+
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                    __html: isi
+                                }}
+                                    className={style.desc}/>
+                            </div>
+                        </div>
+
+                    })
+}
+                </div>
+            </div>
+
         </div>
     )
 }
 
-
-  export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
     const res = await fetch(`https://marifatussalaam.org/Rest_api`)
     const data = await res.json()
     return {
-      props: {data}, // will be passed to the page component as props
+        props: {
+            data
+        }, // will be passed to the page component as props
     }
-  }
+}
 
 export default Home
