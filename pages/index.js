eceec9ef9533f1aec30ffcb1bd94b0f385e9322c
cecepns <cecepns0429@gmail.com>
2020-11-06 11:35:30 +0700

    add meta tag

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,31 @@ function Home({data}) {
     return (
         <div>
             <Head>
-                <title>Ms News - Create Using Next JS </title>
+                <title>Ms News - Create Using Next JS
+                </title>
                 <link rel="icon" href="/favicon.ico"/>
+
+                {/* meta tag */}
+                <meta property="og:locale" content="en_US"/>
+                <meta property="og:type" content="website"/>
+                <meta
+                    property="og:title"
+                    content="SMP AL-QUR'AN MA'RIFATUSSALAAM - TERDEPAN MENJADI SEKOLAH GENERASI BERKARAKTER QURANI"/>
+                <meta
+                    property="og:description"
+                    content="SMP AL-Qur'an Ma'rifatussalaam adalah sekolah boarding terbaik di subang"/>
+                <meta property="og:url" content="https://marifatussalaam.org/"/>
+                <meta
+                    property="og:site_name"
+                    content="SMP AL-QUR'AN MA'RIFATUSSALAAM BOARDING SCHOOL SUBANG"/>
             </Head>
+
+            {/* Website */}
             <Banner/>
             <AboutUs/>
+
             <div className="bg-gray-200 lg:px-12 py-5">
-                    <News news={data}/>
+                <News news={data}/>
             </div>
 
             <Excellence/>
