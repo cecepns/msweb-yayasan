0cb4ba748ed5a9e050a8ff4f86c0334276811044
cecepns <cecepns0429@gmail.com>
2020-11-26 11:08:01 +0700

    add pages eskul

---
+++ components/ekstrakurikuler.js
@@ -0,0 +1,212 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faProductHunt, faBootstrap, faTumblrSquare, faAmilia, faEtsy} from '@fortawesome/free-brands-svg-icons'
+import {faCameraRetro, faBasketballBall, faFutbol, faSwimmer, faTree} from '@fortawesome/free-solid-svg-icons'
+
+function Eskul() {
+    return (
+        <div className="py-12 bg-white mb-4">
+            <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+
+                <div className="mt-2">
+                    <ul className="md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                        <li>
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faProductHunt} size="lg"/>
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">
+                                        Pramuka
+                                    </h4>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                        <FontAwesomeIcon icon={faBootstrap} size="lg"/>
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">Bulu Tangkis</h4>
+
+                                </div>
+                            </div>
+                        </li>
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faBasketballBall} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">
+                                        Basket
+                                    </h4>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faSwimmer} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">Renang</h4>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faTumblrSquare} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">Thibbun Nabawi</h4>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faAmilia} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">Angklung</h4>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faBootstrap} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">Bela Diri</h4>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faAmilia} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">Arabic Club</h4>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faCameraRetro} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">
+                                        Jurnalistik</h4>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faFutbol} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">
+                                        Futsal</h4>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faEtsy} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">English Club</h4>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li className="mt-10 md:mt-0">
+                            <div className="flex">
+                                <div className="flex-shrink-0">
+                                    <div
+                                        className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                                        <FontAwesomeIcon icon={faTree} size="lg"/>
+
+                                    </div>
+                                </div>
+                                <div className="ml-4">
+                                    <h4 className="text-lg mt-2 leading-6 tracking-wide font-medium text-gray-900">Go Green</h4>
+                                </div>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Eskul
---
+++ pages/ekstrakurikuler.js
@@ -0,0 +1,23 @@
+import Ekstrakurikuler from '../components/ekstrakurikuler'
+
+function Eskul() {
+    return (
+        <div className="bg-gray-200 lg:px-12 pt-20 pb-5 w-full h-full">
+            <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 lg:text-center">
+                <h3
+                    className="mt-5 text-3xl tracking-wide leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+                    Ekstrakurikuler
+                </h3>
+                <p
+                    className="my-4 max-w-2xl font-light text-lg leading-7 text-black-500 opacity-75 lg:mx-auto">
+                    Lebih dari 10 Ekstrakurikuler yang akan melatih Softskill mu di Ma'rifatussalaam Qur'anic Boarding School 
+                </p>
+            </div>
+
+            <Ekstrakurikuler/>
+
+        </div>
+    )
+}
+
+export default Eskul
