0642633906ce9f60d21d30af0db44e1f1492f60f
DESKTOP-CJOERL0\LENOVO <cecepns0429@gmail.com>
2021-10-14 13:54:21 +0700

    update title and image

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,31 @@ import Banner from '../components/banner'
 import AboutUs from '../components/aboutUs'
 import VideoProfile from '../components/videoProfile'
 import Testimoni from '../components/testimoni'
+import Head from 'next/head'
 // import Search from '../components/searchList'
 
 function Home({posts}) {
 
     return (
         <div>
-           
+
+            <Head>
+                <title>Ma'rifatussalaam Qur'anic Boarding School</title>
+                <link rel="icon" href="/logo_sekolah.png"/>
+                <meta name="description" content="Ma'rifatussalaam"></meta>
+
+                <meta property="og:title" content="Ma'rifatussalaam"/>
+            </Head>
+
             <Banner/>
-    
+
             <AboutUs/>
 
             <News news={posts}/>
 
             <Excellence/>
 
-            <Testimoni/>
-
-            {/* <Search/> */}
+            <Testimoni/> {/* <Search/> */}
 
         </div>
     )
@@ -35,7 +42,7 @@ export async function getStaticProps(context) {
         props: {
             posts
         }, // will be passed to the page component as props
-        revalidate:10,
+        revalidate: 10
     }
 }
 
