fdc8e476cfa49dac6f00e6ee009a9b776860516b
DESKTOP-CJOERL0\LENOVO <cecepns0429@gmail.com>
2021-10-22 10:55:33 +0700

    update serverside props

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -3,44 +3,33 @@ import {useRouter} from 'next/router'
 import {useState, useEffect} from 'react'
 import Head from 'next/head'
 
+
+export async function getServerSideProps(ctx) {
+
+
+    const id = ctx.query.id
+    const response = await fetch(`https://adminwp.marifatussalaam.org/wp-json/wp/v2/posts?slug=${id}`);
+    const list = await response.json();
+
+    return {
+        props: {
+            list: list
+        }
+    }
+
+}
+
+
 function DetailNews({list}) {
-    // const router = useRouter() let detail = router.query console.log(router)
 
-    const router = useRouter();
-    const id = router.query.id
-
-    const [loading,
-        setLoading] = useState(false)
-
-    const [data,
-        setData] = useState(list)
-
-    useEffect(() => {
-        async function detailNews() {
-            const response = await fetch(`https://adminwp.marifatussalaam.org/wp-json/wp/v2/posts?slug=${id}`);
-            const list = await response.json();
-            setData(list);
-            setLoading(true)
-        }
-
-        if (data.length === 0) {
-            detailNews();
-        } else {
-            setTimeout(() => {
-                setLoading(true)
-            }, 1000);
-        }
-
-    }, [])
 
     let content = null
   
-    if (loading) {
-        if (data.length > 0)
+        if (list.length > 0)
          {
             content = 
                     <div className="container mx-auto mt-20 py-10 px-5">
-                    {data.map(i => {
+                    {list.map(i => {
                         return <div key={i.id} className="flex flex-col items-center">
                              <Head>
                                 <title>{i.title.rendered} </title>
@@ -78,29 +67,13 @@ function DetailNews({list}) {
                 Mohon maaf, berita tidak di temukan.
             </div>
         }
-    } else {
-        content = <div
-            className="h-screen bg-white-300 mx-auto my-auto w-full flex justify-center items-center">
-            loading...
-        </div>
-    }
+   
 
     return (
         <>
             {content}
         </>
     )
-}
-
-DetailNews.getInitialProps = async ctx => {
-    console.log(ctx)
-    if (!ctx.req) {
-        return {list: []}
-    }
-    const id = ctx.query.id
-    const response = await fetch(`https://adminwp.marifatussalaam.org/wp-json/wp/v2/posts?slug=${id}`);
-    const list = await response.json();
-    return {list: list}
 }
 
 export default DetailNews
